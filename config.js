ed9102ed99b55c2afef2cb2095245d045a9955d0
Alex <44865976+alexkolmar@users.noreply.github.com>
2025-08-31 00:13:43 +0300

    edited celebrity database

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js - конфигурация приложения
+const CONFIG = {
+    DEFAULT_GIST_ID: "a69c7957121ab29b4ed27368671fbbda", // ЗАМЕНИТЕ на ваш реальный Gist ID
+    AUTO_LOAD: true, // Автозагрузка данных при запуске
+    SYNC_INTERVAL: 300000 // Интервал синхронизации в ms (5 минут)
+};
